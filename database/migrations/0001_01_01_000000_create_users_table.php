fc3c9d82a6c52b0ebeeda0bc26d6fb1482df72cf
Rezky Desyafa <rezkydesyafa@gmail.com>
2026-01-12 03:21:18 +0700

    refactor migreation

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique()->nullable(); // Added from 2026_01_03_160424
             $table->string('email')->unique();
+            $table->string('role')->default('user'); // Added from 2026_01_03_160424
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/migrations/2026_01_03_161703_create_rekam_medis_table.php
+++ database/migrations/2026_01_03_161703_create_rekam_medis_table.php
@@ -9,25 +9,38 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-   public function up(): void
-{
-    Schema::create('rekam_medis', function (Blueprint $table) {
-        $table->id('id_rm'); // Primary Key Custom
-        // Relasi ke Pasien & Dokter
-        $table->foreignId('pasien_id')->constrained('pasiens', 'id_pasien')->onDelete('cascade');
-        $table->foreignId('dokter_id')->constrained('dokters', 'id_dokter')->onDelete('cascade');
-        
-        $table->date('tgl_kunjungan');
-        
-        // Data Medis (SOAP)
-        $table->text('keluhan');       // Subjective
-        $table->string('tensi')->nullable();    // Objective
-        $table->integer('berat_badan')->nullable();
-        $table->integer('suhu')->nullable();
-        $table->text('diagnosa');      // Assessment
-        $table->text('catatan_tambahan')->nullable(); // Plan
-        
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('rekam_medis', function (Blueprint $table) {
+            $table->id('id_rm'); // Primary Key Custom
+            
+            // Relasi ke Pasien & Dokter
+            $table->foreignId('pasien_id')->constrained('pasiens', 'id_pasien')->onDelete('cascade');
+            $table->foreignId('dokter_id')->constrained('dokters', 'id_dokter')->onDelete('cascade');
+            
+            $table->date('tgl_kunjungan');
+            
+            // Data Medis (SOAP)
+            $table->text('keluhan');       // Subjective
+            $table->string('tensi')->nullable();    // Objective
+            $table->integer('berat_badan')->nullable();
+            $table->integer('tinggi_badan')->nullable(); // Added from 2026_01_11_083306
+            $table->integer('suhu')->nullable();
+            
+            $table->text('diagnosa');      // Assessment
+            $table->text('catatan_tambahan')->nullable(); // Plan
+            
+            $table->enum('status_cetak', ['Belum Dicetak', 'Sudah Dicetak'])->default('Belum Dicetak'); // Added from 2026_01_12_014607
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rekam_medis');
+    }
 };
